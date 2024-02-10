baf9da6117493eb6dee4a410b377048204904931
ManonLacombe <115247796+ManonLacombe@users.noreply.github.com>
2024-02-10 19:25:01 +0100

    suite commentaire tâche A

--- Sources/TaskA/TaskATempMapper.java
+++ Sources/TaskA/TaskATempMapper.java
@@ -18,10 +18,14 @@ public class TaskATempMapper extends Mapper<LongWritable, Text, Text, DoubleWrit
 
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException 
     {
+        //Condition 1er caractère de la ligne commençant par N
         if(value.getString().charAt(0).equals("N"))
         {
+            //Division des éléments contenu dans value
             String[] values = value.getString().split(";"); // N ; CodeUE ; Année ; NumEtudiant ; Note
+            //Création de la nouvelle clé de type Année;(3ers caractère)CodeUE;NumEtudiant
             String newKey = values[2] + ";" + values[1].substr(0, 3) + ";" + values[3];
+            //renvoye de la nouvelle clé associée aux notes
             context.write(new Text(newKey), new DoubleWritable(Double.parseDouble(values[4])));
         }
     }
